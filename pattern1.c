87690456a02f2ce451f7a076eb7e59992129f141
riyagoel930 <113978413+riyagoel930@users.noreply.github.com>
2022-09-27 19:11:27 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+        printf("*");
+        }
+        printf("\n");
+    }
+    getch();
+}
+
+
+
+//
+
+output:
+*****
+*****
+*****
+*****
+
+//
